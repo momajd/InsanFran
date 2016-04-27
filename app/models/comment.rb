876e14ffbb639116c602642904b1170e895d24a9
momajd <mo.a.majd@gmail.com>
2016-04-27 10:27:26 -0700

    models and associations for posts, comments, likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
+  validates: :body, :user_id, :post_id, presence: true
 
+  belongs_to :post
+  belongs_to :user
 end
---
+++ app/models/like.rb
@@ -0,0 +1,6 @@
+class Like < ActiveRecord::Base
+  validates: :user_id, :post_id, presence: true
+
+  belongs_to :user
+  belongs_to :post
+end
