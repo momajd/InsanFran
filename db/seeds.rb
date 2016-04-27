b5728c7c76a783e0b0e78727d9cae7a6560c417f
momajd <mo.a.majd@gmail.com>
2016-04-26 21:51:16 -0700

    added guest to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+guest = User.create!(
+  username: "guest",
+  password: "password",
+  name: "Guest"
+)
