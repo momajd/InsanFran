187496fb9255e2870399952a000421e82366da15
momajd <mo.a.majd@gmail.com>
2016-04-28 20:51:05 -0700

    new seed data

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates :image_url, :user_id, presence: true
 
   belongs_to :user
-  has_many :comments
-  has_many :likes
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-guest = User.create!(
-  username: "guest",
-  password: "password",
-  name: "Guest"
-)
+guest = User.create!([
+  {username: "guest", password: "password", name: "Guest"},
+  {username: "momajd", password: "password", name: "Mo"}
+])
+
+posts = Post.create!([
+  {image_url: "https://upload.wikimedia.org/wikipedia/commons/5/54/Golden_Gate_Bridge_0002.jpg",
+  user_id: 1},
+  {image_url: "https://upload.wikimedia.org/wikipedia/commons/d/dc/San_Francisco_-_Oakland_Bay_Bridge_At_Night.jpg",
+  user_id: 2}
+])
+
+comments = Comment.create!([
+  {user_id: 2, post_id: 1, body: "first comment oh yea"},
+  {user_id: 2, post_id: 1, body: "second comment"}
+  ])
