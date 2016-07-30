792dc9f59002e89c4053268274178b79b95629c4
momajd <mo.a.majd@gmail.com>
2016-07-30 18:25:06 -0400

    update video

--- frontend/components/auth/BackVideo.jsx
+++ frontend/components/auth/BackVideo.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var BackVideo = React.createClass({
 
   render: function () {
-    var videoUrl = "https://s3.amazonaws.com/insanfran/insanfran2.mp4";
+    var videoUrl = "https://s3.amazonaws.com/insanfran/insanfran.mp4";
     return (
       <div className='video-container'>
         <video autoPlay muted loop preload className="video">
